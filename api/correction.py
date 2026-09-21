3e9a829d50124077c8b85db08abc16a4e7e44436
PuffyCripsy <PuffyCripsy >
2026-09-21 20:32:00 +0900

    Restore my original API key

--- api/correction.py
+++ api/correction.py
@@ -11,7 +11,7 @@ class handler(BaseHTTPRequestHandler):
             user_input = data.get('diary', '')
 
             # 코딧세이 API 설정
-            api_key = "codyssey-7m8q9p2r5n1k4j3l"
+            api_key = "=sk-cody-live-cR3j_c3DeW4jE3ubMquNcfI84I4b8ra8NuzMLBs9NEM"
             url = "https://copa.codyssey.kr/v1/chat/completions"
 
             headers = {
